13b4c72890ce365cbbbd2112364dee0688b4f0fb
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-06-09 17:40:22 +0500

    integrate DRF and create course API

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import CourseSerializer
+from .models import Course
 # Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset=Course.objects.all().order_by('-created_at')
+    serializer_class=CourseSerializer

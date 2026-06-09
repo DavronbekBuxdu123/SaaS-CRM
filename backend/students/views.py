1bacdf77095807db5defe58e62d1394ff01244c8
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-06-09 21:45:01 +0500

    add students api logic

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework import viewsets
 # Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset=Student.objects.all().order_by('-joined_at')
+    serializer_class=StudentSerializers

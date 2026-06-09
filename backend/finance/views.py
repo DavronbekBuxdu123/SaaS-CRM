bb3ace7784970c597df2a9f61fd25e62d6ba56b0
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-06-09 22:01:44 +0500

    complete all API endpoints

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Payment
+from rest_framework import viewsets
+from .serializers import PaymentSerializers
 # Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset=Payment.objects.all().order_by('-paid_at')
+    serializer_class=PaymentSerializers
--- backend/students/serializers.py
+++ backend/students/serializers.py
@@ -3,4 +3,5 @@ from rest_framework import serializers
 
 class StudentSerializers(serializers.ModelSerializer):
     class Meta:
+        model=Student
         fields="__all__"
